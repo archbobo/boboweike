448a561d504f15e338fe3e90d00e8a4adbe194ff
架构师波波 <william.yang@boboweike.cn>
2024-12-13 17:30:01 +0800

    chore: adds clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
-
-export default api.withTRPC(MyApp);
