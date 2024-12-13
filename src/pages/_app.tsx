75ba550f7c42bbf82902f2de1adfccb9d66e57bc
架构师波波 <william.yang@boboweike.cn>
2024-12-13 17:55:46 +0800

    fix export

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
@@ -9,3 +10,5 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     </ClerkProvider>
   );
 };
+
+export default api.withTRPC(MyApp);
