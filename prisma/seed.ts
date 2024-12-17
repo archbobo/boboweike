8f1de4010d2f41a2dce429dee80a815dc616773c
架构师波波 <william.yang@boboweike.cn>
2024-12-17 18:14:06 +0800

    things

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import uuidByString from 'uuid-by-string';
+
 const prisma = new PrismaClient();
 async function main() {
   const trashId = uuidByString('trash');
@@ -7,16 +8,23 @@ async function main() {
     where: { id: trashId },
     update: {},
     create: {
-      email: 'bobo@boboweike.cn',
+      email: 'chris@typehero.dev',
       name: 'chris',
       Challenge: {
         create: {
           createdAt: new Date(),
           updatedAt: new Date(),
           name: "trash's first challenge",
+          description: `### trash is a cool guy
+          - he likes to eat trash
+          - he likes to sleep in trash
+          - he likes to be trash
+          `,
           prompt: `/* _____________ Test Cases _____________ */
 Extends<HelloWorld, \`Hello, \${string}\`>()
+
 Extends<HelloWorld, \`\${string}!\`>()
+
 /* _____________ Your Code Here _____________ */
 type HelloWorld =`,
           difficulty: 'EASY',
@@ -30,14 +38,20 @@ type HelloWorld =`,
     where: { id: gId },
     update: {},
     create: {
-      email: 'g@boboweike.cn',
+      email: 'g@typehero.dev',
       name: 'g',
       Challenge: {
         create: {
           createdAt: new Date(),
           updatedAt: new Date(),
           name: "g's first challenge",
+          description: `### g is a cool guy
+          - he does not like to eat trash
+          - he does not like to sleep in trash
+          - he doesn not like to be trash
+          `,
           prompt: `Equal<T, 1>();
+
 type T =`,
           difficulty: 'EASY',
           upvotes: 420,
