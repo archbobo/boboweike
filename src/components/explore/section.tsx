1a709a1cb31e6c38363c3ee26a854fdd555c155e
架构师波波 <william.yang@boboweike.cn>
2024-12-19 15:51:51 +0800

    responsive layout & focus states

--- src/components/explore/section.tsx
+++ src/components/explore/section.tsx
@@ -2,16 +2,18 @@ import Link from 'next/link';
 import type { Challenges } from '.';
 import { TypographyH3 } from '../ui/typography/h3';
 import { ExploreCard } from './explore-card';
+
 interface Props {
   title: string;
   data(): Challenges;
 }
+
 export async function ExploreSection({ title, data }: Props) {
   const challenges = await data();
   return (
     <div>
       <TypographyH3 className="mb-3">{title}</TypographyH3>
-      <div className="grid grid-cols-3 gap-2">
+      <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
         {challenges.map((challenge) => (
           <Link
             className="group focus:outline-none"
--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -35,8 +35,9 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:bg-accent focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-accent/50 data-[active]:bg-accent/50 h-10 py-2 px-4 group w-max',
+  'inline-flex items-center justify-center rounded-xl text-sm font-medium transition-colors focus:outline-none focus:bg-accent focus:text-accent-foreground disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-accent/50 data-[active]:bg-accent/50 h-10 py-2 px-4 group w-max',
 );
+
 const NavigationMenuTrigger = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
@@ -48,7 +49,7 @@ const NavigationMenuTrigger = React.forwardRef<
   >
     {children}{' '}
     <ChevronDown
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+      className="relative top-[1px] ml-3 h-4 w-4 transition duration-200 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
   </NavigationMenuPrimitive.Trigger>
