55ccd15171ea0d330f21423a29604e95fda484f4
架构师波波 <william.yang@boboweike.cn>
2024-12-20 18:42:50 +0800

    Add a format script and changed the eslint rule for quotes

--- src/components/explore/explore-card.tsx
+++ src/components/explore/explore-card.tsx
@@ -51,19 +51,14 @@ const rtf = new Intl.RelativeTimeFormat('en', {
   // TODO: style: 'short' on 640px - 720px
 });
 
-// TODO: typescript
+// get relevant time from a date object
 const getRelativeTime = (date: Date) => {
-  const now = new Date();
-  // @ts-ignore
-  const elapsed = date - now;
+  const elapsed = date.getTime() - Date.now();
 
-  // "Math.abs" accounts for both "past" & "future" scenarios
-  // @ts-ignore
-  for (const u in units) {
-    // @ts-ignore
-    if (Math.abs(elapsed) > units[u] || u == 'second')
-      // @ts-ignore
-      return rtf.format(Math.round(elapsed / units[u]), u);
+  for (const [unit, ms] of Object.entries(units)) {
+    if (Math.abs(elapsed) > ms || unit === 'second') {
+      return rtf.format(Math.round(elapsed / ms), unit as Intl.RelativeTimeFormatUnit);
+    }
   }
 };
 
