d83a4db466e5471cc8a8c88a7019433212009aff
架构师波波 <william.yang@boboweike.cn>
2024-12-21 18:24:31 +0800

    basic submissions tab

---
+++ src/components/challenge/save-submission.action.ts
@@ -0,0 +1,19 @@
+
+'use server';
+import { prisma } from '~/server/db';
+
+export async function saveSubmission(
+  challengeId: number,
+  userId: string,
+  code: string,
+  isSuccessful: boolean,
+) {
+  await prisma.solution.create({
+    data: {
+      challengeId,
+      userId,
+      code,
+      isSuccessful,
+    },
+  });
+}
