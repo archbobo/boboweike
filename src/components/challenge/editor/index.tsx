ca68ac4b56547c5f6d36cd52556b04d535828f43
架构师波波 <william.yang@boboweike.cn>
2024-12-21 17:54:32 +0800

    Json -> Text column

--- src/components/challenge/editor/index.tsx
+++ src/components/challenge/editor/index.tsx
@@ -95,15 +95,13 @@ export const CodePanel = (props: Props) => {
       return props.challenge.prompt;
     }
 
-    const [appendSolutionToThis, separator] = (props.challenge.prompt as string).split(
-      USER_CODE_START_REGEX,
-    );
-    const parsedUserSolution = JSON.parse(usersExistingSolution?.code as string) as string;
+    const [appendSolutionToThis, separator] = props.challenge.prompt.split(USER_CODE_START_REGEX);
+    const parsedUserSolution = usersExistingSolution?.code;
 
     return `${appendSolutionToThis ?? ''}${separator ?? ''}${parsedUserSolution}`;
   }, [props.challenge?.Solution, props.challenge?.prompt, props.mode, props.prompt]);
 
-  const [code, setCode] = useState(defaultCode as string);
+  const [code, setCode] = useState(defaultCode);
 
   const editorTheme = theme === 'light' ? 'vs' : 'vs-dark';
   const modelRef = useRef<monaco.editor.ITextModel>();
@@ -135,11 +133,7 @@ export const CodePanel = (props: Props) => {
               action: <ToastAction altText="Try again">Try again</ToastAction>,
             });
           } else {
-            await saveSubmission(
-              props.challenge.id,
-              session?.user?.id as string,
-              JSON.stringify(solution) ?? '',
-            );
+            await saveSubmission(props.challenge.id, session?.user?.id as string, solution ?? '');
             router.refresh();
             toast({
               variant: 'success',
