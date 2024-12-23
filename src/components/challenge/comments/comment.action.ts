760d4e40cb9ca94ebeaef9f493f88af5566adf6b
架构师波波 <william.yang@boboweike.cn>
2024-12-23 19:49:32 +0800

    explore card redesign & other fixes

--- src/components/challenge/comments/comment.action.ts
+++ src/components/challenge/comments/comment.action.ts
@@ -6,10 +6,10 @@ import { prisma } from '~/server/db';
 
 export async function addChallengeComment(challengeId: number, text: string) {
   const session = await getServerSession(authOptions);
-  
+
   if (!session?.user.id) return 'unauthorized';
   if (text.length === 0) return 'text_is_empty';
-  
+
   return await prisma.challengeComment.create({
     data: {
       challengeId,
