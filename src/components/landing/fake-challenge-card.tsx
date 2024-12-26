1d9eb1838780a6f2d39b384a961c972508e70af3
架构师波波 <william.yang@boboweike.cn>
2024-12-26 18:48:01 +0800

    shiny smooth animations on the landing cards

--- src/components/landing/fake-challenge-card.tsx
+++ src/components/landing/fake-challenge-card.tsx
@@ -1,9 +1,21 @@
 import { clsx } from 'clsx';
+import { motion, stagger, type AnimationProps, type SVGMotionProps } from 'framer-motion';
+
+function getRandomWidth() {
+  const min = 100;
+  const range = 240 - min;
+  let rand = Math.random();
+  rand = Math.floor(Math.random() * range);
+  rand = rand + min;
+
+  return rand;
+}
 
 export type FakeChallengeCardProps = {
   title: string;
   className?: string;
   difficulty?: 'EASY' | 'MEDIUM' | 'HARD';
+  animation?: AnimationProps;
 };
 
 const COLORS_BY_DIFFICULTY = {
@@ -14,29 +26,50 @@ const COLORS_BY_DIFFICULTY = {
   EXTREME: 'dark:bg-orange-300 bg-orange-600',
 };
 
-export const FakeChallengeCard = ({ title, className, difficulty }: FakeChallengeCardProps) => {
+const codeLineProps = {
+  width: 0,
+  rx: '4.5',
+  height: 10,
+  initial: { width: 0 },
+  transition: { duration: 0.5, delay: 0.15 },
+} as SVGMotionProps<SVGRectElement>;
+
+export const FakeChallengeCard = ({
+  title,
+  className,
+  difficulty,
+  animation,
+}: FakeChallengeCardProps) => {
   return (
-    <div
+    <motion.div
+      {...animation}
+      whileHover={{
+        scale: 1.06,
+        skew: -2,
+        transition: { delay: 0, duration: 0.2, type: 'keyframes' },
+      }}
       className={clsx(className, {
-        'group/card flex w-[320px] flex-col gap-3 rounded-2xl border border-neutral-300 bg-zinc-100 p-7 drop-shadow-[0_0_15px_rgba(49,49,49,0.2)] duration-300 hover:-skew-x-6 hover:shadow-[2rem_2rem_2rem_-1rem_#0004,inset_1rem_1rem_4rem_-1rem_#fff1] dark:border-zinc-800 dark:bg-zinc-900 dark:drop-shadow-[0_0_15px_rgba(49,49,49,0.35)] dark:hover:border-zinc-600':
+        'card-container group/card flex w-[320px] select-none flex-col gap-3 rounded-2xl border border-neutral-300 bg-zinc-100 p-7 drop-shadow-[0_0_15px_rgba(49,49,49,0.2)] transition-shadow duration-500 hover:shadow-[2rem_2rem_2rem_-1rem_#0004,inset_1rem_1rem_4rem_-1rem_#fff1] dark:border-zinc-800 dark:bg-zinc-900 dark:drop-shadow-[0_0_15px_rgba(49,49,49,0.35)]':
           true,
       })}
     >
       <p className="translate-x-1 text-lg font-medium">{title}</p>
       <div className="flex gap-3">
         <div
-          className={`inline-flex w-fit translate-x-1 items-center rounded-full border border-transparent px-2.5 py-0.5 text-xs font-semibold text-primary-foreground duration-300 group-hover/card:-translate-x-1 group-hover/card:-translate-y-1 group-hover/card:shadow-[0.75rem_0.75rem_1rem_-0.25rem_#0008] ${COLORS_BY_DIFFICULTY[difficulty || 'HARD']}`}
+          className={`inline-flex w-fit translate-x-1 items-center rounded-full border border-transparent px-2.5 py-0.5 text-xs font-semibold text-primary-foreground ${
+            COLORS_BY_DIFFICULTY[difficulty || 'HARD']
+          }`}
         >
           {difficulty || 'HARD'}
         </div>
-        <div className="flex w-fit items-center justify-center rounded-full bg-neutral-300 bg-transparent px-2.5 py-1 pl-1.5 pr-2 text-xs font-bold text-neutral-700 duration-300 group-hover/card:-translate-x-2 group-hover/card:-translate-y-1 group-hover/card:shadow-[0.75rem_0.75rem_1rem_-0.25rem_#0008] dark:bg-zinc-700 dark:text-white">
+        <div className="flex w-fit items-center justify-center rounded-full bg-neutral-300 bg-transparent px-2.5 py-1 pl-1.5 pr-2 text-xs font-bold text-neutral-700 duration-300 dark:bg-zinc-700 dark:text-white">
           @you
         </div>
       </div>
       <div className="translate-x-1 text-xs">
         Implement the built-in <code>Pick</code> type.
       </div>
-      <div className="mt-4 h-56 flex-grow rounded-xl bg-zinc-300/70 p-4 duration-300 group-hover/card:-translate-x-3 group-hover/card:-translate-y-3 group-hover/card:shadow-[1rem_1rem_2.5rem_-1rem_#0008] dark:bg-zinc-800/70">
+      <div className="mt-4 h-56 flex-grow rounded-xl bg-zinc-300/70 p-4 dark:bg-zinc-800/70">
         <svg
           width="256"
           height="193"
@@ -44,18 +77,69 @@ export const FakeChallengeCard = ({ title, className, difficulty }: FakeChalleng
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <rect width="234" height="9" rx="4.5" fill="#2B2B32" />
-          <rect y="109" width="234" height="9" rx="4.5" fill="#2B2B32" />
-          <rect y="90" width="234" height="9" rx="4.5" fill="#2B2B32" />
-          <rect y="19" width="123" height="9" rx="4.5" fill="#544048" />
-          <rect y="38" width="142" height="9" rx="4.5" fill="#544048" />
-          <rect y="128" width="130" height="9" rx="4.5" fill="#544048" />
-          <rect y="147" width="189" height="9" rx="4.5" fill="#544048" />
-          <rect y="57" width="142" height="9" rx="4.5" fill="#404F54" />
-          <rect y="166" width="142" height="9" rx="4.5" fill="#404F54" />
-          <rect y="184" width="142" height="9" rx="4.5" fill="#404F54" />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="0"
+            fill="#2B2B32"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="19"
+            fill="#544048"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="38"
+            fill="#544048"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="57"
+            fill="#404F54"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="90"
+            fill="#2B2B32"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="109"
+            fill="#2B2B32"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            y="128"
+            fill="#544048"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="147"
+            fill="#544048"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="166"
+            fill="#404F54"
+          />
+          <motion.rect
+            {...codeLineProps}
+            animate={{ width: getRandomWidth() }}
+            y="184"
+            fill="#404F54"
+          />
         </svg>
       </div>
-    </div>
+    </motion.div>
   );
 };
