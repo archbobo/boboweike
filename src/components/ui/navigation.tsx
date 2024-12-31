a671fc42b455a396f0600940db5f270d58e72f7f
架构师波波 <william.yang@boboweike.cn>
2024-12-31 18:32:08 +0800

    explore carousel and sum stuff

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -60,11 +60,7 @@ export function Navigation() {
                 </defs>
               </svg>
 
-              <span className="font-bold leading-3">
-                type
-                <br />
-                hero
-              </span>
+              <span className="font-bold leading-3">波波微课</span>
             </a>
 
             <Link href="/explore">
