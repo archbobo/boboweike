741c546800106436009620c805e9e797425c746c
架构师波波 <william.yang@boboweike.cn>
2025-01-04 18:36:34 +0800

    Cook

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -34,7 +34,21 @@ if (!process.env.GITHUB_SECRET) {
   throw new Error('No GITHUB_SECRET has been provided.');
 }
 
+const useSecureCookies = Boolean(process.env.VERCEL_URL);
+
 export const authOptions: NextAuthOptions = {
+  cookies: {
+    sessionToken: {
+      name: `${useSecureCookies ? '__Secure-' : ''}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        domain: 'boboweike.cn',
+        secure: useSecureCookies,
+      },
+    },
+  },
   callbacks: {
     redirect: ({ url, baseUrl }) => {
       if (url.startsWith('/')) return `${baseUrl}${url}`;
@@ -100,26 +114,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-};
-
-const useSecureCookies = Boolean(process.env.VERCEL_URL);
-// ok
-const appHostname = new URL(`https://${process.env.VERCEL_URL}` ?? 'http://127.0.0.1').hostname;
-
-export const adminAuthOptions: NextAuthOptions = {
-  ...authOptions,
-  cookies: {
-    sessionToken: {
-      name: `${useSecureCookies ? '__Secure-' : ''}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        domain: appHostname,
-        secure: useSecureCookies,
-      },
-    },
-  },
 };
 
 /**
