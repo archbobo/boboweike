7bf94f09652f24a2ea4245b1618407d73d0b31d9
架构师波波 <william.yang@boboweike.cn>
2025-01-05 15:19:29 +0800

    change to env vars

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html suppressHydrationWarning lang="en">
       <head>
-        <title>Type Hero Admin</title>
+        <title>波波微课管理后台</title>
       </head>
       <body className={`${inter.className} flex flex-col`}>
         <Providers>
