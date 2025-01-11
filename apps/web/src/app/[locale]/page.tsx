ad965cc8a0f036eb56d324ed2600247dd7ea4bfc
架构师波波 <william.yang@boboweike.cn>
2025-01-11 20:05:26 +0800

    chore: remove default exports from non entry points

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Footsies } from '~/components/ui/footsies';
-import Features from './_components/features';
-import Hero from './_components/hero';
+import { Features } from './_components/features';
+import { Hero } from './_components/hero';
 import { WaitlistBanner } from './_components/waitlist-banner';
 
 export const metadata: Metadata = {
