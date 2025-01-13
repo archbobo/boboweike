d38ae4443f0a339fe53c651856b7fe720b3dbd26
架构师波波 <william.yang@boboweike.cn>
2025-01-13 16:27:03 +0800

    feat(web): static rendering on

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -3,6 +3,8 @@ import { Footsies } from '~/components/ui/footsies';
 import { Features } from './_components/features';
 import { Hero } from './_components/hero';
 import { WaitlistBanner } from './_components/waitlist-banner';
+import { getStaticParams } from '~/locales/server';
+import { setStaticParamsLocale } from 'next-international/server';
 
 export const metadata: Metadata = {
   title: '波波微课',
@@ -10,7 +12,13 @@ export const metadata: Metadata = {
     'Connect, collaborate, and grow with a community of TypeScript developers. Elevate your skills through interactive coding challenges, discussions, and knowledge sharing',
 };
 
-export default async function Index() {
+export function generateStaticParams() {
+  return getStaticParams();
+}
+
+export default async function Index({ params: { locale } }: { params: { locale: string } }) {
+  setStaticParamsLocale(locale);
+
   return (
     <>
       <Hero />
--- apps/web/src/app/[locale]/waitlist/page.tsx
+++ apps/web/src/app/[locale]/waitlist/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
-export { Waitlist as default } from './_components';
-
 export const metadata: Metadata = {
   title: 'Waitlist | 波波微课',
   description: 'Join the waitlist for 波波微课 and be the first to know when we launch!',
 };
+
+export { Waitlist as default } from './_components';
