cddbc4a94dce4ce2820439261057923bb377becc
架构师波波 <william.yang@boboweike.cn>
2025-01-13 18:15:23 +0800

    feat: linkable comments

--- packages/monaco/src/code-editor.tsx
+++ packages/monaco/src/code-editor.tsx
@@ -6,7 +6,7 @@ import { useTheme } from 'next-themes';
 import { useMemo } from 'react';
 import { useEditorSettingsStore } from './settings-store';
 import { libSource } from './editor-types';
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports, import/no-extraneous-dependencies
 import * as MonacoEditor from 'monaco-editor';
 
 const ADMIN_HOST = 'admin.boboweike.cn';
