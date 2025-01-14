2b27b5f1a8fd1cb6ca0ff0bde720115ff702640b
架构师波波 <william.yang@boboweike.cn>
2025-01-14 16:33:19 +0800

    feat: allow solution comments to be shared - Closes

--- apps/web/src/app/[locale]/challenge/[id]/solutions/page.tsx
+++ apps/web/src/app/[locale]/challenge/[id]/solutions/page.tsx
@@ -1,11 +1,9 @@
-import type { Session } from '@repo/auth/server';
 import { getServerAuthSession } from '@repo/auth/server';
-import { prisma } from '@repo/db';
 import { notFound } from 'next/navigation';
 import { Solutions } from './_components';
 import { getChallengeRouteData } from '../getChallengeRouteData';
 import { buildMetaForChallenge } from '~/app/metadata';
-import { cache } from 'react';
+import { getSolutionsRouteData } from './getSolutionRouteData';
 
 interface Props {
   params: {
@@ -16,7 +14,7 @@ interface Props {
 export async function generateMetadata({ params: { id } }: Props) {
   const challenge = await getChallengeRouteData(id, null);
   return buildMetaForChallenge({
-    title: `Solutions to ${challenge.name} | 波波微课`,
+    title: `Solutions to ${challenge.name} | TypeHero`,
     description: challenge.shortDescription,
     username: challenge.user.name,
   });
@@ -24,7 +22,7 @@ export async function generateMetadata({ params: { id } }: Props) {
 
 export default async function SolutionPage({ params: { id } }: Props) {
   const session = await getServerAuthSession();
-  const solutions = await getSolutionsData(id, session);
+  const solutions = await getSolutionsRouteData(id, session);
 
   if (!solutions) {
     return notFound();
@@ -32,44 +30,3 @@ export default async function SolutionPage({ params: { id } }: Props) {
 
   return <Solutions challenge={solutions} />;
 }
-
-export type ChallengeSolutionsRouteData = NonNullable<Awaited<ReturnType<typeof getSolutionsData>>>;
-export const getSolutionsData = cache(async (challengeId: string, session: Session | null) => {
-  const data = await prisma.challenge.findFirst({
-    where: { id: Number(challengeId) },
-    select: {
-      id: true,
-      // we just want their most recent solution
-      submission: {
-        where: {
-          userId: session?.user.id || '',
-        },
-        orderBy: [
-          {
-            createdAt: 'desc',
-          },
-        ],
-        take: 1,
-      },
-      sharedSolution: {
-        orderBy: [
-          {
-            createdAt: 'desc',
-          },
-        ],
-        include: {
-          user: {
-            select: {
-              name: true,
-            },
-          },
-          _count: {
-            select: { vote: true, solutionComment: true },
-          },
-        },
-      },
-    },
-  });
-
-  return data;
-});
