533921e551269703da8e725b9ffc75383d352e00
架构师波波 <william.yang@boboweike.cn>
2025-01-14 20:32:30 +0800

    fix lint issue

--- apps/web/tests/e2e/home.test.ts
+++ apps/web/tests/e2e/home.test.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('home page', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.getByRole('heading', { name: '波波 微课' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '波波微课' })).toBeVisible();
 });
