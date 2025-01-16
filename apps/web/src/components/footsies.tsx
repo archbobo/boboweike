ee3bd42b24463d7b71cae4f6746249816012dfcf
架构师波波 <william.yang@boboweike.cn>
2025-01-16 13:00:15 +0800

    Footer adjustments

--- apps/web/src/components/footsies.tsx
+++ apps/web/src/components/footsies.tsx
@@ -4,27 +4,28 @@ import Link from 'next/link';
 export function Footsies() {
   return (
     <footer className="flex flex-col items-center text-sm font-light sm:px-16 md:px-0">
-      <div className="container my-4 flex flex-col justify-between gap-6 px-10 pb-2 md:my-12 lg:flex-row">
-        <div className="space-x-2">
-          Built with <Binary className="inline-block h-5 w-5 text-[#31bdc6]" /> by 架构师杨波
+      <div className="container my-4 flex flex-col justify-between gap-2 px-10 pb-2 md:my-12 lg:flex-row lg:items-end">
+        <div>
+          Built with <Binary className="inline-block h-5 w-5 text-[#31bdc6]" /> by 架构师杨波.
+          <div className="mt-2 text-neutral-500 dark:text-neutral-400">
+            <Link
+              href="/privacy"
+              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+            >
+              Privacy Policy
+            </Link>{' '}
+            |{' '}
+            <Link
+              href="/tos"
+              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+            >
+              Terms of Service
+            </Link>
+          </div>
         </div>
-        <div className="text-neutral-500 dark:text-neutral-400">
-          <Link
-            href="/privacy"
-            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-          >
-            Privacy Policy
-          </Link>{' '}
-          |{' '}
-          <Link
-            href="/tos"
-            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-          >
-            Terms of Service
-          </Link>
-        </div>
-        <div className="text-neutral-500 dark:text-neutral-400">
-          © {new Date().getFullYear()} 波波微课
+        <div className="text-neutral-500 lg:text-right dark:text-neutral-400">
+          <div className="inline-block rotate-180">©</div>
+          {new Date().getFullYear()} 波波微课
         </div>
       </div>
     </footer>
