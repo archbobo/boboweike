576d61806b5f05da22bd587fcc2b873d12fd6b3d
架构师波波 <william.yang@boboweike.cn>
2025-01-17 18:23:22 +0800

    feat(web): add more static routes

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -3,16 +3,11 @@ import { Footsies } from '~/components/footsies';
 import { Features } from './_components/features';
 import { Hero } from './_components/hero';
 import { WaitlistBanner } from './_components/waitlist-banner';
-import { getStaticParams } from '~/locales/server';
 import { setStaticParamsLocale } from 'next-international/server';
 import { buildMetaForDefault } from '../metadata';
 
 export async function generateMetadata(): Promise<Metadata> {
   return buildMetaForDefault({});
-}
-
-export function generateStaticParams() {
-  return getStaticParams();
 }
 
 export default async function Index({ params: { locale } }: { params: { locale: string } }) {
