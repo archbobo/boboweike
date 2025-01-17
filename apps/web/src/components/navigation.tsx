0aa6c3d734ce186d0b3116c779f913ef56867280
架构师波波 <william.yang@boboweike.cn>
2025-01-17 18:08:41 +0800

    fix lint issue

--- apps/web/src/components/navigation.tsx
+++ apps/web/src/components/navigation.tsx
@@ -199,12 +199,12 @@ function LoginButton() {
           </a>
         ) : null}
         {isAdminOrMod ? (
-          <a className="block" href="/challenge-playground">
+          <Link className="block" href="/challenge-playground">
             <DropdownMenuItem className="focus:bg-accent rounded-lg p-2 duration-300 focus:outline-none dark:hover:bg-neutral-700/50">
               <Play className="mr-2 h-4 w-4" />
               <span>Challenge Playground</span>
             </DropdownMenuItem>
-          </a>
+          </Link>
         ) : null}
         <DropdownMenuSeparator />
         <Button
