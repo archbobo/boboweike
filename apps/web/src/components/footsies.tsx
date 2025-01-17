5c97f2db6e31e26b55a0e96305484a3ecfd47051
架构师波波 <william.yang@boboweike.cn>
2025-01-17 17:14:22 +0800

    Footer socials

--- apps/web/src/components/footsies.tsx
+++ apps/web/src/components/footsies.tsx
@@ -1,32 +1,52 @@
-import { Binary } from '@repo/ui/icons';
+import { Binary, Github, Twitter } from '@repo/ui/icons';
 import Link from 'next/link';
 
 export function Footsies() {
   return (
-    <footer className="flex flex-col items-center text-sm font-light sm:px-16 md:px-0">
-      <div className="container my-4 flex flex-col justify-between gap-2 px-10 pb-2 md:my-12 lg:flex-row lg:items-end">
-        <div>
+    <footer className="flex flex-col items-center gap-2 px-8 pb-12 text-sm font-light sm:px-16 sm:pb-20 sm:pt-6 md:px-0 md:py-12">
+      <div className="container flex flex-col-reverse justify-between gap-2 md:flex-row md:items-end">
+        <span>
           Built with <Binary className="inline-block h-5 w-5 text-[#31bdc6]" /> by 架构师杨波.
-          <div className="mt-2 text-neutral-500 dark:text-neutral-400">
-            <Link
-              href="/privacy"
-              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-            >
-              Privacy Policy
-            </Link>{' '}
-            |{' '}
-            <Link
-              href="/tos"
-              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-            >
-              Terms of Service
-            </Link>
-          </div>
+        </span>
+        <div className="flex gap-2">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="group gap-1 md:inline-flex"
+            href="https://github.com/boboweike"
+          >
+            <Github className="h-4 w-4 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="group gap-1 md:inline-flex"
+            href="https://twitter.com/boboweikeapp"
+          >
+            <Twitter className="h-4 w-4 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white" />
+          </a>
         </div>
-        <div className="text-neutral-500 lg:text-right dark:text-neutral-400">
+      </div>
+      <div className="container flex flex-col justify-between gap-2 text-neutral-500 md:flex-row  md:items-end dark:text-neutral-400">
+        <span>
+          <Link
+            href="/privacy"
+            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+          >
+            Privacy Policy
+          </Link>{' '}
+          |{' '}
+          <Link
+            href="/tos"
+            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+          >
+            Terms of Service
+          </Link>
+        </span>
+        <span>
           <div className="inline-block rotate-180">©</div>
           {new Date().getFullYear()} 波波微课
-        </div>
+        </span>
       </div>
     </footer>
   );
