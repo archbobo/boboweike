52c369fe37b622c8e642d728eb7e0c92c3992e4b
架构师波波 <william.yang@boboweike.cn>
2025-01-17 15:03:17 +0800

    Server side nav logic

--- apps/web/src/app/[locale]/[username]/page.tsx
+++ apps/web/src/app/[locale]/[username]/page.tsx
@@ -11,13 +11,16 @@ export default function Page({ params }: Props) {
   return <Profile username={params.username} />;
 }
 
-export async function generateMetadata({ params: { username } }: Props) {
-  const name = decodeURIComponent(username).substring(1);
-  if (name)
+export async function generateMetadata({ params: { username: usernameFromQuery } }: Props) {
+  const [, username] = decodeURIComponent(usernameFromQuery).split('@');
+
+  if (!username) return {};
+
+  if (username)
     return buildMetaForUser({
-      username: name,
-      title: `${name}'s profile | 波波微课`,
-      description: `View the profile of ${name} on 波波微课.`,
+      username,
+      title: `${username}'s profile | 波波微课`,
+      description: `View the profile of ${username} on 波波微课.`,
     });
   return buildMetaForDefault({
     title: 'Profile | 波波微课',
