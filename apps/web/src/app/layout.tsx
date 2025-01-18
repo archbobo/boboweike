37ebaa941bb09aa29a7787291fb5fdb9d5a941fc
架构师波波 <william.yang@boboweike.cn>
2025-01-18 13:51:26 +0800

    Fix meta with this template thing

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: {
+    template: '波波微课',
+    default: 'Home',
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
