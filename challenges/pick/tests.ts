ae3a7f959cd70e80ea7968235284acb39a256705
架构师波波 <william.yang@boboweike.cn>
2025-01-19 11:06:22 +0800

    chore: switch challenge to use MyPick

--- challenges/pick/tests.ts
+++ challenges/pick/tests.ts
@@ -9,28 +9,30 @@ interface Pokemon {
   resilience: string;
 }
 
-const pickYourPokemon = (pokemon: Pick<Pokemon, 'name' | 'type'>) => {
+const pickYourPokemon = (
+  pokemon: MyPick<Pokemon, 'name' | 'type'>
+) => {
   const { type, name } = pokemon;
-  return `You picked the ${type}-type Pokemon ${name}!`;
-};
+  return `You picked the ${type}-type Pokemon ${name}!`
+}
 
 const pikachu = {
   name: 'Pikachu',
   type: 'Electric',
-};
+}
 
-console.log(pickYourPokemon(pikachu));
+console.log(pickYourPokemon(pikachu))
 // => `You picked the Electric-type Pokemon Pikachu!`
 
 /** Selecting an invalid property should be an error. */
 const pokemonAttacks = (
   // @ts-expect-error
-  pokemon: Pick<Pokemon, 'attacks' | 'age'>,
+  pokemon: MyPick<Pokemon, 'attacks' | 'age'>
 ) => {
   return 'Oops! WE ';
-};
+}
 
 /** It's also totally fine to select a single property */
-const recallPokemon = (pokemon: Pick<Pokemon, 'name'>) => {
-  return `You recalled ${pokemon.name}!`;
-};
+const recallPokemon = (pokemon: MyPick<Pokemon, 'name'>) => {
+  return `You recalled ${pokemon.name}!`
+}
