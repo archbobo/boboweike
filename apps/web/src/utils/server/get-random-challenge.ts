48a71cfa04ced99bb55776d7d5b3846c5841f57c
架构师波波 <william.yang@boboweike.cn>
2025-01-20 16:26:45 +0800

    fix postgresql queryRaw issue

--- apps/web/src/utils/server/get-random-challenge.ts
+++ apps/web/src/utils/server/get-random-challenge.ts
@@ -6,9 +6,9 @@ interface resultType {
 }
 
 export async function getRandomChallenge() {
-  const result: resultType[] =
-    await prisma.$queryRaw`SELECT slug FROM Challenge ORDER BY RAND() LIMIT 1`;
-
+  const result: resultType[] = await prisma.$queryRaw<{ slug: string }[]>`
+    SELECT slug FROM "Challenge" ORDER BY RANDOM() LIMIT 1
+  `;
   if (result.length > 0 && result[0] && Boolean(result[0].slug)) {
     return result[0].slug;
   }
