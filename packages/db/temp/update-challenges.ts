39f02714cfe173e0552ed88b404e4eb31439629f
架构师波波 <william.yang@boboweike.cn>
2025-01-21 12:46:22 +0800

    fix: update challenge script to use author metadata

--- packages/db/temp/update-challenges.ts
+++ packages/db/temp/update-challenges.ts
@@ -1,6 +1,5 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import uuidByString from 'uuid-by-string';
 import { ingestChallenges } from '../seed/data/challenge-ingest';
 import { prisma } from '../src';
 
@@ -10,28 +9,30 @@ const __dirname = path.dirname(__filename);
 const challengePath = path.join(__dirname, '../../../challenges');
 
 export const slugify = (str: string) => str.toLowerCase().replace(/\s/g, '-');
-const BOBOWEIKE_ID = uuidByString('boboweike');
 
 try {
-  const boboWeikeUser = await prisma.user.findFirstOrThrow({
-    where: { id: BOBOWEIKE_ID },
-  });
-
   const challengesToUpdateOrCreate = await ingestChallenges(challengePath);
 
   const transactions = [];
   for (const challenge of challengesToUpdateOrCreate) {
+    const author = await prisma.user.findFirstOrThrow({
+      where: {
+        name: challenge.author,
+      },
+    });
+    const { author: _, ...challengeWithoutAuthor } = challenge;
     transactions.push(
       prisma.challenge.upsert({
         where: {
           slug: challenge.slug,
         },
         update: {
-          ...challenge,
+          ...challengeWithoutAuthor,
+          userId: author.id,
         },
         create: {
-          ...challenge,
-          userId: boboWeikeUser.id,
+          ...challengeWithoutAuthor,
+          userId: author.id,
         },
       }),
     );
