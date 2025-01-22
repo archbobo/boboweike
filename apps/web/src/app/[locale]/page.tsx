e33700d00b38bfe977e183b55762ec302e637598
架构师波波 <william.yang@boboweike.cn>
2025-01-22 16:36:35 +0800

    chore: make homepage static agagin

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -3,6 +3,11 @@ import { Footsies } from '~/components/footsies';
 import { Features } from './_components/features';
 import { Hero } from './_components/hero';
 import { WaitlistBanner } from './_components/waitlist-banner';
+import { getStaticParams } from '~/locales/server';
+
+export function generateStaticParams() {
+  return getStaticParams();
+}
 
 export default async function Index({ params: { locale } }: { params: { locale: string } }) {
   setStaticParamsLocale(locale);
