48d398df31a10849a52ac7375142a4c8b340d079
架构师波波 <william.yang@boboweike.cn>
2025-01-27 09:40:57 +0800

    chore: add wrapped CTA

--- apps/web/src/app/[locale]/aot-2023/_components/index.tsx
+++ apps/web/src/app/[locale]/aot-2023/_components/index.tsx
@@ -1,47 +1,58 @@
 import { Button } from '@repo/ui/components/button';
 import { Github } from '@repo/ui/icons';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { getAllFlags } from '~/utils/feature-flags';
 import { CountdownTimer } from '../../_components/countdown-timer';
 import { About } from './about';
 import { CardGrid } from './card-grid';
+import { Suspense } from 'react';
+import { AOTCardSkeleton } from './aot-card-skeleton';
 
 export async function AotLandingPage() {
   const featureFlags = await getAllFlags();
   if (!featureFlags.enableHolidayEvent) return notFound();
 
   return (
-    <div className="flex flex-col gap-5 pb-8 md:gap-10 md:py-5">
-      <div className="container">
-        <div className="relative mx-auto w-fit text-center">
-          <div className="absolute left-0 top-0 -z-10 hidden h-full w-full transform-gpu bg-gradient-to-r from-red-500 via-red-600 to-green-500 opacity-70 blur-3xl sm:opacity-40 dark:block" />
-          <h1 className="mb-10 mt-16 text-4xl font-bold tracking-tighter text-black sm:text-8xl dark:text-white">
-            <span>Advent</span> of <span className="text-red-600">TypeScript</span>
-          </h1>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-5">
-          <CountdownTimer />
-          <div className="flex w-full flex-col items-center justify-center gap-2 md:w-auto md:flex-row md:gap-5">
-            <About className="w-full md:w-auto" />
-            <Button
-              asChild
-              className="flex w-full items-center justify-center gap-2 rounded-xl border-2 px-4 py-2 md:w-auto dark:text-white"
-              variant="outline"
-            >
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="gap-1 md:inline-flex"
-                href="https://github.com/boboweike/boboweike"
+    <Suspense fallback={<AOTCardSkeleton />}>
+      <div className="flex flex-col gap-5 pb-8 md:gap-10 md:py-5">
+        <div className="container">
+          <div className="relative mx-auto w-fit text-center">
+            <div className="absolute left-0 top-0 -z-10 hidden h-full w-full transform-gpu bg-gradient-to-r from-red-500 via-red-600 to-green-500 opacity-70 blur-3xl sm:opacity-40 dark:block" />
+            <h1 className="mb-10 mt-16 text-4xl font-bold tracking-tighter text-black sm:text-8xl dark:text-white">
+              <span>Advent</span> of <span className="text-red-600">TypeScript</span>
+            </h1>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-5">
+            <CountdownTimer />
+            <div className="flex w-full flex-col items-center justify-center gap-2 md:w-auto md:flex-row md:gap-5">
+              <Link
+                href="/aot-2023/wrapped"
+                className="group flex w-full items-center justify-center gap-2 rounded-xl bg-red-600 px-3 py-2 text-sm font-bold text-white duration-300 md:mx-auto md:w-auto"
               >
-                <Github className="h-4 w-4" />
-                Star us on GitHub
-              </a>
-            </Button>
+                See the results!
+              </Link>
+              <About className="w-full md:w-auto" />
+              <Button
+                asChild
+                className="flex w-full items-center justify-center gap-2 rounded-xl border-2 px-4 py-2 md:w-auto dark:text-white"
+                variant="outline"
+              >
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="gap-1 md:inline-flex"
+                  href="https://github.com/boboweike/boboweike"
+                >
+                  <Github className="h-4 w-4" />
+                  Star us on GitHub
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
+        <CardGrid />
       </div>
-      <CardGrid />
-    </div>
+    </Suspense>
   );
 }
